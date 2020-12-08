4c31c34ba5ee2cfc3222e13298c78a9a18d9be37
Timothy Wangwe <wangwetymothy@gmail.com>
2020-12-08 17:22:47 +0300

    Introduce while loop to test error handling better

--- source/week-1/error-handling/runtime.py
+++ source/week-1/error-handling/runtime.py
@@ -1,11 +1,21 @@
-x = 42
-y = 0
+# Error Handling
 try:
-    print(x / y)
-except ZeroDivisionError as e:
-    # Optionally, log e somewhere
-    print('Sorry, something went wrong')
+    print(x)
+except NameError:
+    print("Variable x is not defined")
 except:
-    print('Something really went wrong')
+    print("Something else went wrong")
 finally:
     print('This always runs on success or failure')
+
+# Use while to create an infinite loop that will run
+# # until false is returned (correct input in this case)
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
+# Instead of letting python to shout at the user "ValueError"
+# we catch the anticipated error and display a polite message
