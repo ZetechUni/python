1f64b6e5d4531512638a84dfe1809d6fcfc200d6
Timothy Wangwe <wangwetymothy@gmail.com>
2020-12-08 16:59:08 +0300

    Add strftime() time formatting method

--- source/week-1/dates/format_date.py
+++ source/week-1/dates/format_date.py
@@ -8,10 +8,15 @@ today = datetime.now()
 # to display only part of the date
 # All these functions return integers
 # Convert them to strings before concatenating them to another string
-print('Day: ' + str(today.day))
+print('Date: ' + str(today.day))
 print('Month: ' + str(today.month))
 print('Year: ' + str(today.year))
 
 print('Hour: ' + str(today.hour))
 print('Minute: ' + str(today.minute))
 print('Second: ' + str(today.second))
+
+# Using the strftime() Method
+print("DD/MM/YY: " + str(today.strftime('%d/%m/%y')))
+print("DD Month YYYY: " + str(today.strftime('%d %B %Y')))
+print("MM/DD/YYYY, H:M:S : " + str(today.strftime("%m/%d/%Y, %H:%M:%S")))
