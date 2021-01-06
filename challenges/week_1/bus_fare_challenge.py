966bb870411ecbe01f2220eca928f98e31ada344
Timothy Wangwe <wangwetymothy@gmail.com>
2021-01-06 16:26:52 +0300

    Move instructions to readme file

--- challenges/week_1/bus_fare_challenge.py
+++ challenges/week_1/bus_fare_challenge.py
@@ -1,16 +1,1 @@
-# Write a program that does the following:
-# - gets today's date and stores it in a variable 'date'
-# - uses today's date to get the name on the day of the week written in short form with
-#   the first letter capitalized eg. 'Fri' if today were Friday
-# - uses if statements to determine the today's fare following these bus fare schedule:
-#       - monday - friday --> 100
-#       - saturday --> 60
-#       - sunday --> 80
-# - Prints the results in this format
-#       Date:    2021-01-05
-#       Day:     Tue
-#       Fare:    100
-# NOTE THAT YOUR PROGRAM MUST MAKE USE OF THE VARIABLE NAME : 'date', 'day' and 'fare'.
-# FAILURE TO WHICH ALL YOUR TESTS WILL FAIL.
-
-# WRITE YOUR CODE HERE
+# WRITE YOUR CODE SOLUTION HERE
