57b67044f66ee1955fdba7c9d197388b1d7efff4
Alexandrina Likova <48678880+alexlikova@users.noreply.github.com>
2021-01-27 20:47:01 +0000

    Update format_strings.py

--- source/week-1/variables/format_strings.py
+++ source/week-1/variables/format_strings.py
@@ -1,8 +1,21 @@
+# FIRST WAY
+# the capitalize function will return the string with 
+# the first letter uppercase and the rest of the word lowercase
+
+# Ask the user for their first and last name // it does not matter how the user types it:
+# ALEX, alex, aLEX etc. --> it will be saved in the memory as Alex (Fist letter will be with uppercase)
+first_name = input('What is your first name? ').capitalize()
+last_name = input('What is your last name? ').capitalize()
+
+print('Hello ' + first_name + ' ' + last_name)
+
+
+# SECOND WAY
 # Ask the user for their first and last name
 first_name = input('What is your first name? ')
 last_name = input('What is your last name? ')
 
 # the capitalize function will return the string with 
 # the first letter uppercase and the rest of the word lowercase
-print ('Hello ' + first_name.capitalize() + ' ' \
-       + last_name.capitalize())
+print('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
+
