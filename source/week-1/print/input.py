58c5ee0b1eb2b78a5e0331f45868beca7763740f
Alexandrina Likova <48678880+alexlikova@users.noreply.github.com>
2021-01-27 20:14:24 +0000

    Update input.py
    # IMPORTANT: When you declare a variable, it has to start with small letter
    The user can type their name on the next line with the \n command / not on the same line as above

--- source/week-1/print/input.py
+++ source/week-1/print/input.py
@@ -1,6 +1,12 @@
-# The input funciton allows you to prompt the user for a value
+# The input function allows you to prompt the user to type something
 
 # You need to declare a variable to hold the value entered by the user
-Name = input('What is your name? ')
+# IMPORTANT: When you declare a variable, it has to start with small letter
+name = input('What is your name? ')
 
-print('Great, your name is '+ Name)
+print('Great, your name is ' + name)
+
+# The user can type their name on the next line with the \n command / not on the same line as above
+name = input("What is your name?\n")
+
+print('Great, your name is ' + name)
