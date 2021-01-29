4c717f7730c7c859964e4262d3b674865d7fbc12
Martin Kinaro <njengamartin72@gmail.com>
2021-01-29 12:23:52 +0300

    week one bus fare challenge.

--- challenges/week_1/bus_fare_challenge.py
+++ challenges/week_1/bus_fare_challenge.py
@@ -1,1 +1,23 @@
-# WRITE YOUR CODE SOLUTION HERE
+from datetime import date
+
+date = date.today()
+
+day = date.strftime("%a")
+
+bus_fare = {
+    "Mon":100,
+    "Tue":100,
+    "Wen":100,
+    "Thur":100,
+    "Fri":100,
+    "Sat":60,
+    "Sun":80
+}
+fare = None
+if day in bus_fare:
+    print(date)
+    print(day)
+    fare = bus_fare[day]
+    print(fare)
+
+
